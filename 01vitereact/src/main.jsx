4529cf0730d26f5ee05cae288c5533293602837b
my name <pandeyabhimanyu10@gmail.com>
2025-01-23 16:22:08 +0530

    custom props in react folder

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | chai </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href:'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to Visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  reactElement
 )
